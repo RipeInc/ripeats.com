e7e70014c3a8629cc141025e1a46cf55ac1edb9a
= <qg231@nyu.edu>
2015-11-08 16:27:14 -0500

    Correct backbone installation

---
+++ app/assets/javascripts/ripe_com.js
@@ -0,0 +1,13 @@
+window.RipeCom = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  RipeCom.initialize();
+});
