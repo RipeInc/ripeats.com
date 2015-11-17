4165946d33aad113454531f3994a6537e7cf145f
= <qg231@nyu.edu>
2015-11-16 21:02:33 -0500

    Fix main page showup

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -2,6 +2,27 @@ RipeCom.Routers.Router = Backbone.Router.extend({
   routes: {
     "main": "showSearch",
     "login": "showLogin",
-    "signup": "showSignup",
+    "signup": "showSignup"
+  },
+
+  initialize: function(options){
+    this.$rootEl = options.$rootEl;
+  },
+
+  showSearch: function(){
+    var searchView = new RipeCom.Views.SplashSearch();
+    this._swap(searchView);
+  },
+
+  showLogin: function(){
+  },
+
+  showSignup: function(){
+  },
+
+  _swap: function(view){
+    this._currentView && this._currentView.remove();
+    this._currentView = view;
+    this.$rootEl.html(view.render().$el);
   }
 })
