5d0a47a58e2b1c61d55d9c1e80cdff97144b791b
= <qg231@nyu.edu>
2015-11-21 17:08:24 -0500

    Add login sheet

--- app/assets/javascripts/views/splash_login.js
+++ app/assets/javascripts/views/splash_login.js
@@ -3,16 +3,28 @@ RipeCom.Views.SplashLogin = Backbone.FusedView.extend({
   className: 'splash-main',
 
   events: {
-
+    "click .form-switch-user": "swapToUser",
+    "click .form-switch-restaurant": "swapToRestaurant",
   },
 
   initialize: function(options){
-
+    this.userFormTemplate = JST['signin_user'];
+    this.restaurantFormTemplate = JST['signup_restaurant'];
   },
 
   render: function(){
     var content = this.template();
     this.$el.html(content);
     return this;
+  },
+
+  swapToUser: function(){
+    var content = this.userFormTemplate();
+    this.$el.find("#main-form-holder").html(content);
+  },
+
+  swapToRestaurant: function(){
+    var content = this.restaurantFormTemplate();
+    this.$el.find("#main-form-holder").html(content);
   }
 })
