8d639481a26e2a1c9d591e57bf9c5fbf0eadd489
= <qg231@nyu.edu>
2015-11-24 11:04:43 -0500

    Write user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+      redirect_to user_url(@user)
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def edit
+
+  end
+
+  def delet
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: "static_pages#index"
+
+  resources :user, only: [:show, :create]
+
+  namespace :api, defaults: { format: :json } do
+    resources :user, only: [:show, :create]
+  end
 end
