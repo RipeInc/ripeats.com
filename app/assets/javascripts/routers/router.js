4a6b1d7e0905867b40ab9bfb33a25e542d798e31
= <qg231@nyu.edu>
2016-01-07 15:45:42 -0500

    Reveal search bar

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -34,6 +34,7 @@ RipeCom.Routers.Router = Backbone.Router.extend({
   _swap: function(view){
     this._currentView && this._currentView.remove();
     this._currentView = view;
+    $("#ripe-overlay-field-master").css("display", "none");
     this.$rootEl.html(view.render().$el);
   }
 })
