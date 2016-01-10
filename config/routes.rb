101861049de3301d015869fff5243e5a7547e38f
= <qg231@nyu.edu>
2016-01-10 17:51:22 -0500

    Add mission fluff page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "static_pages#index"
+  get "/mission", to: "static_pages#mission"
 
   resources :users, only: [:show, :create]
   resources :corporates, only: [:show]
